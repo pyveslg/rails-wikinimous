f6ddfd12212cd63b5190c14a9adbe7345a36977f
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-05-16 16:25:12 +0200

    Wikinimous - all routes set and css done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    #
+  end
+
+  def edit
+    #
+  end
+
+  def update
+    @article.update(set_params)
+    redirect_to article_path(@article)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    new_article = Article.create(set_params)
+    redirect_to article_path(new_article)
+  end
+
+  def destroy
+    @article.delete
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def set_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, :content, presence: true
+
+end
